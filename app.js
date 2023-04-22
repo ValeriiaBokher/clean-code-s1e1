3ae9bd1857a6013ec9142a1b0dd7e1ece23f7446
Valeriia Bokher <valeriiabokher@gmaol.com>
2023-04-22 14:38:53 +0300

    refactor(basic-3.4): delete extra selectors-part2

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="delete-img"
     deleteButton.appendChild(deleteButtonImg);
 
 
